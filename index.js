39c0caa6f5eabd775bba715e6a1951a6efcdcfc9
Rene Henzinger <r.henzinger@easysolution.eu>
2019-04-23 17:00:52 +0200

    bugfix

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ module.exports = function (source) {
 	var loaderUtils;
 	this.cacheable && this.cacheable(true);
 	loaderUtils = require("loader-utils");
-	var FilePreprocessor = require('file-preprocessor');
+	var FilePreprocessor = require('file-preprocessor').FilePreprocessor;
+	const util = require('util');
 	query = loaderUtils.parseQuery(this.query);
 	try {
 		var fp = new FilePreprocessor(query);
-		return fp.ProcessString(source);
+		return fp.processString(source);
 	} catch (e) {
 		this.emitError('ERROR: ' + e)
 		throw e;
